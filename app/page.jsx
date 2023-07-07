f9bcfce1cea21d439a2233ce3d30adcf6a5a42b6
zeeshan <zeeshan.mustafa@emerald-labs.com>
2023-07-07 16:08:52 +0500

    header text added in home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,16 @@
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-      Home
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient">
+          AI-Powered Prompts
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is open-source AI prompting tool for the modern wold to discover, create and share creative prompts
+      </p>
     </section>
   );
 };
