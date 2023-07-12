70878a64962789974903e4a1d379b72620cb108c
zeeshan <mirzazeeshanmusatafa1@yahoo.com>
2023-07-12 16:44:11 +0500

    connection made with mongo checking session checking user session

--- app/api/auth/[...next]/route.js
+++ app/api/auth/[...next]/route.js
@@ -1,19 +1,47 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-
+import { connectToDB } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET_ID,
-    })
+    }),
   ],
   async session([session]) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
 
+    sessionUser.user.id = sessionUser._id.toString();
+
+    return session;
   },
-  async signIn({profile}) {
-   }
-})
 
-export {handler as GET, handler as POST};
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      let userExits = await User.findOne({
+        email: profile.email,
+      });
+
+      if (!userExits) {
+        await User.create({
+          email: profile.email,
+          ussernam: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+});
+
+export { handler as GET, handler as POST };
